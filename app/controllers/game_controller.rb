8c5ef2a3e794fec69dfc911f6ed26e132e1d48c6
alathans <alathans@users.noreply.github.com>
2016-04-22 17:37:53 -0500

    update1

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -5,7 +5,11 @@ class GameController < ApplicationController
 
 # can add all our code that we did in here for RPS... make a nice website
 
-    render("zebra.html.erb")
+  if @computer_move == "rock"
+    @result = "tied"
+  end
+
+  render("zebra.html.erb")
 
   end
 
