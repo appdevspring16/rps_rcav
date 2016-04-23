e897f71d055bd7f89732712a1785fbda00886b5a
Hannah <hannah.meyer@chicagobooth.edu>
2016-04-23 13:18:10 -0500

    Just need to set a host URL

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,13 +2,18 @@ class GameController < ApplicationController
   def rock
     @computer_move = ["rock","scissors","paper"].sample
     @computer_move_format = @computer_move.capitalize
-    @computer_move_image = @computer_move+'.png'
     if @computer_move == "rock"
       @result = "tied"
+      @computer_move_image = "/assets/images/rock.png"
+      @span = "label-warning"
     elsif @computer_move == "paper"
       @result = "lose"
+      @computer_move_image = "/assets/images/paper.png"
+      @span = "label-danger"
     elsif @computer_move == "scissors"
       @result = "win"
+      @computer_move_image = "/assets/images/scissors.png"
+      @span = "label-success"
     end
     render("rock.html.erb")
   end
@@ -18,10 +23,16 @@ class GameController < ApplicationController
     @computer_move_format = @computer_move.capitalize
     if @computer_move == "paper"
       @result = "tied"
+      @computer_move_image = "/assets/images/paper.png"
+      @span = "label-warning"
     elsif @computer_move == "rock"
       @result = "win"
+      @computer_move_image = "/assets/images/rock.png"
+      @span = "label-success"
     elsif @computer_move == "scissors"
       @result = "lose"
+      @computer_move_image = "/assets/images/scissors.png"
+      @span = "label-danger"
     end
     render("paper.html.erb")
   end
@@ -31,10 +42,16 @@ class GameController < ApplicationController
     @computer_move_format = @computer_move.capitalize
     if @computer_move == "scissors"
       @result = "tied"
+      @computer_move_image = "/assets/images/scissors.png"
+      @span = "label-warning"
     elsif @computer_move == "paper"
       @result = "win"
+      @computer_move_image = "/assets/images/paper.png"
+      @span = "label-success"
     elsif @computer_move == "rock"
       @result = "lose"
+      @computer_move_image = "/assets/images/rock.png"
+      @span = "label-danger"
     end
     render("scissors.html.erb")
   end
