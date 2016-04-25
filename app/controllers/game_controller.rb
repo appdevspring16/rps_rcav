4d437c4a704ca34a7d8f268d301bbff4df17b9e2
alathans <alathans@users.noreply.github.com>
2016-04-24 20:52:01 -0500

    rock in there

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,16 +1,17 @@
 class GameController < ApplicationController
 
-  def rock
-  @computer_move = ["rock","paper","scissors"].sample
+  def welcome
+    render("welcome.html.erb")
+  end
 
 # can add all our code that we did in here for RPS... make a nice website
-
+  def rock
+  @computer_move = ["rock","paper","scissors"].sample
   if @computer_move == "rock"
     @result = "tied"
   end
 
   render("zebra.html.erb")
-
   end
 
 end
