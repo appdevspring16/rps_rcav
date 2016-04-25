72ba335bc7a052a5e03a641482a4148fe3b6452a
tburkett88 <airburkett@AirBurketts-MacBook-Air.local>
2016-04-25 00:03:27 -0500

    finished

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,57 +4,80 @@ class GameController < ApplicationController
   def scissors
     @user_move = "scissors"
     @cpu_move=["rock", "paper", "scissors"].sample
+    @cpu_text=@cpu_move.capitalize
+    @cpu_image="http://localhost:3000/assets/images/" + @cpu_move + ".png"
+
     if @user_move == @cpu_move
       @result = "TIED"
+      @user_render = "success"
+      @panel_render = "warning"
+      @cpu_render ="danger"
     elsif @cpu_move == "paper"
       @result = "YOU WON!"
+      @user_render = "success"
+      @cpu_render ="danger"
+      @panel_render = "success"
     else
       @result = "YOU LOST"
-    render("paper.html.erb")
-
-    render("scissors.html.erb")
+      @user_render ="danger"
+      @cpu_render ="success"
+      @panel_render = "danger"
     end
+    render("scissors.html.erb")
   end
 
   def paper
     @user_move = "paper"
     @cpu_move=["rock", "paper", "scissors"].sample
+    @cpu_text=@cpu_move.capitalize
+    @cpu_image="http://localhost:3000/assets/images/" + @cpu_move + ".png"
+
     if @user_move == @cpu_move
       @result = "TIED"
+      @user_render = "success"
+      @panel_render = "warning"
+      @cpu_render ="danger"
     elsif @cpu_move == "rock"
       @result = "YOU WON!"
+      @user_render = "success"
+      @panel_render = "success"
+      @cpu_render ="danger"
     else
       @result = "YOU LOST"
-    render("paper.html.erb")
+      @user_render ="danger"
+      @cpu_render ="success"
+      @panel_render = "danger"
     end
+    render("paper.html.erb")
   end
 
   def rock
     @user_move = "rock"
     @cpu_move=["rock", "paper", "scissors"].sample
+    @cpu_text=@cpu_move.capitalize
     @cpu_image="http://localhost:3000/assets/images/" + @cpu_move + ".png"
 
     if @user_move == @cpu_move
       @result = "TIED"
+      @user_render = "success"
+      @panel_render = "warning"
+      @cpu_render ="danger"
     elsif @cpu_move == "scissors"
       @result = "YOU WON!"
+      @user_render = "success"
+      @panel_render = "success"
+      @cpu_render ="danger"
     else
       @result = "YOU LOST"
+      @user_render ="danger"
+      @cpu_render ="success"
+      @panel_render = "danger"
     end
 
-    if @result == "YOU WON!"
-      @user_render == "panel-success"
-      @cpu_render =="danger"
-    elsif @result == "YOU LOST"
-      @user_render =="danger"
-      @cpu_render =="success"
-    else
-      @user_render == "success"
-      @cpu_render =="success"
+
     render("rock.html.erb")
   end
 
 
-  end
 
 end
