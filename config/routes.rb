bc3a34c97b35bf61e6987be52cd3a069dd0588b2
remustomici <remus.tomici@users.noreply.github.com>
2016-04-25 20:26:46 -0700

    mainly done
    a few formatting colors left

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,13 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  get("/rock", { :controller => "game", :action => "user_plays_rock"})
+
+  get("/paper", { :controller => "game", :action => "user_plays_paper"})
+
+  get("/scissors", { :controller => "game", :action => "user_plays_scissors"})
+
+  get("/", { :controller => "game", :action => "user_plays_rock"})
+
 end
