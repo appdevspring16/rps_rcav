a8fc3530ef0cb364cfa2587d829beed1a54b0032
dangrana <danmgrana@users.noreply.github.com>
2016-04-26 05:53:28 -0500

    monday night

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,13 +1,15 @@
 class GameController < ApplicationController
   def user_plays_rock
-    @computer_move = ["rock","paper","scissors"].sample
+    @computer_move = ["Rock","Paper","Scissors"].sample
 
-    if @computer_move == "rock"
+    if @computer_move == "Rock"
       @result = "Tie"
-    elsif @computer_move == "paper"
+    elsif @computer_move == "Paper"
       @result = "Lose"
     else
       @result = "Win"
+      # @label = "label label-success"
+      @label = '<span class="label label-success">'
     end
       # render("zebra.html.erb")
       render("user_plays_rock.html.erb")
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
 get("/rock", {:controller=>"game", :action=>"user_plays_rock"})
 end
 
