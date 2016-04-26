eb976a6cedbb27452c715af1fc3b5075fc513159
chrisfcallaway <chrisfcallaway@gmail.com>
2016-04-26 18:16:22 -0500

    callaway-chris-in-progress
    work in progress

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,9 @@
+class GameController < ApplicationController
+
+	def rock
+
+
+		render("zebra.html.erb")
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+get("/play/rock", { :controller => "game", :action => "rock" })
+
+
 end
