08af1e3fa59a2303491e3474305f07791e7ba2ea
Maayan <maharon@>
2016-04-28 15:40:35 -0500

    rcav_final

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,14 +2,44 @@ class GameController <ApplicationController
   def user_plays_rock
     @computer_move = ["rock", "paper", "scissors"].sample
 
-if @computer_move== "rock"
-  @result = 'Tie'
-elsif @computer_move=="paper"
-  @result="Lose"
-else
-  @result = "win"
-end
+    if @computer_move== "rock"
+      @result = 'Tie'
+    elsif @computer_move=="paper"
+      @result="Lose"
+    else
+      @result = "win"
+    end
 
-    render("zebra.html.erb")
+    render("rock.html.erb")
   end
+
+  def user_plays_paper
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move== "paper"
+      @result = 'Tie'
+    elsif @computer_move=="scissors"
+      @result="Lose"
+    else
+      @result = "win"
+    end
+
+    render("paper.html.erb")
+  end
+
+  def user_plays_scissors
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move== "scissors"
+      @result = 'Tie'
+    elsif @computer_move=="rock"
+      @result="Lose"
+    else
+      @result = "win"
+    end
+
+    render("scissors.html.erb")
+  end
+
+
 end
