afef5a8e21f0f2fbb47a17fa8d0594818ebfa306
Alex Frederick <afreder0@users.noreply.github.com>
2016-04-27 22:33:13 -0500

    good-stopping-point

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,11 +1,36 @@
 class GameController < ApplicationController
   def rock
-
-@computer_move= ["rock", "paper", "scissors"].sample
-
-if @computer_move=="rock"
-  @result="You tied"
+    @computer_move= ["rock", "paper", "scissors"].sample
+    if @computer_move=="rock"
+      @result="You tied"
+    elsif @computer_move=="paper"
+      @result="You lose"
+    else @result="You win"
 end
-  render("zebra.html.erb")
+    render("rock.html.erb")
+end
+
+def paper
+
+  @computer_move= ["rock", "paper", "scissors"].sample
+
+  if @computer_move=="rock"
+    @result="You win"
+  elsif @computer_move=="paper"
+    @result="You tied"
+  else @result="You lose"
   end
+  render("paper.html.erb")
+end
+
+def scissors
+  @computer_move= ["rock", "paper", "scissors"].sample
+  if @computer_move=="rock"
+    @result="You lose"
+  elsif @computer_move=="paper"
+    @result="You win"
+  else @result="You tied"
+  end
+  render("scissors.html.erb")
+end
 end
