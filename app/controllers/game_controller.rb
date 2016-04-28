3d34f691b723d92cbc24799f3f2b8bf340b7d1c9
Josh Charney <jfcharney@user.noreply.github.com>
2016-04-28 18:04:11 -0500

    Final
    Ready for feedback

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,43 +6,69 @@ class GameController < ApplicationController
 
 
   def user_plays_rock
-    @computer_move = ["rock", "paper", "scissors"].sample
+    @computer_move = ["Rock", "Paper", "Scissors"].sample
 
-    if @computer_move == "rock"
-      @result = "tie"
-    elsif @computer_move == "paper"
-      @result = "lose"
+    if @computer_move == "Rock"
+      @result = "Tie"
+      # @user_label = "panel-danger"
+      @image = "/assets/images/rock.png"
+      @banner = "label-warning"
+    elsif @computer_move == "Paper"
+      @result = "Lose"
+      # @user_label = "panel-danger"
+      @image = "/assets/images/paper.png"
+      @banner = "label-danger"
     else
-      @result = "win"
-
+      @result = "Win"
+      # @user_label = "panel-success"
+      @image = "/assets/images/scissors.png"
+      @banner = "label-success"
     end
     render("rock.html.erb")
   end
 
 
   def user_plays_paper
-    @computer_move = ["rock", "paper", "scissors"].sample
+    @computer_move = ["Rock", "Paper", "Scissors"].sample
 
-    if @computer_move == "paper"
-      @result = "tie"
-    elsif @computer_move == "scissors"
-      @result = "lose"
+    if @computer_move == "Paper"
+      @result = "Tie"
+      # @user_label = "panel-danger"
+      @image = "/assets/images/paper.png"
+      @banner = "label-warning"
+    elsif @computer_move == "Scissors"
+      @result = "Lose"
+      # @user_label = "panel-danger"
+      @image = "/assets/images/scissors.png"
+      @banner = "label-danger"
     else
-      @result = "win"
+      @result = "Win"
+      # @user_label = "panel-success"
+      @image = "/assets/images/rock.png"
+      @banner = "label-success"
     end
 
     render("paper.html.erb")
   end
 
   def user_plays_scissors
-    @computer_move = ["rock", "paper", "scissors"].sample
+    @computer_move = ["Rock", "Paper", "Scissors"].sample
 
-    if @computer_move == "scissors"
-      @result = "tie"
-    elsif @computer_move == "rock"
-      @result = "lose"
+    if @computer_move == "Scissors"
+      @result = "Tie"
+      # @user_label = "panel-danger"
+      @image = "/assets/images/scissors.png"
+      @banner = "label-warning"
+    elsif @computer_move == "Paper"
+      @result = "Win"
+      # @user_label = "panel-success"
+      @image = "/assets/images/paper.png"
+      @banner = "label-success"
     else
-      @result = "win"
+      @result = "Lose"
+      # @user_label = "panel-success"
+      @image = "/assets/images/rock.png"
+      @banner = "label-danger"
     end
     render("scissors.html.erb")
   end
