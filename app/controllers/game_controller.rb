caec0320b700bbb22418d20bbfa8b2177163e2b1
Zachary C. Cornett <zcornett@chicagobooth.edu>
2016-04-27 23:24:03 -0500

    Rough Start
    Wow.  I totally forgot to save intermenttedly.

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,43 @@
+class GameController < ApplicationController
+  def user_plays_rock
+    @computer_move=["rock","paper","scissors"].sample
+
+if @computer_move=="rock"
+  @result="Tie"
+elsif @computer_move=="paper"
+  @result="Lose"
+else
+  @result="Win"
+end
+    render("rock.html.erb")
+  end
+
+
+  def user_plays_paper
+    @computer_move=["rock","paper","scissors"].sample
+
+if @computer_move=="rock"
+  @result="Win"
+elsif @computer_move=="paper"
+  @result="Tie"
+else
+  @result="Lose"
+end
+    render("paper.html.erb")
+  end
+
+  def user_plays_scissors
+    @computer_move=["rock","paper","scissors"].sample
+
+  if @computer_move=="rock"
+  @result="Lose"
+  elsif @computer_move=="paper"
+  @result="Win"
+  else
+  @result="Tie"
+  end
+    render("scissors.html.erb")
+  end
+
+
+end
