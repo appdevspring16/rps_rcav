17a4b6c0209c5bfda3442b0240c81d36d6a88050
Kenny Moyer <kmoyer@users.noreply.github.com>
2016-04-28 10:00:38 -0400

    kenny-moyer-ready-for-review

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "game", :action => "user_plays_rock"})
+
   get("/rock", {:controller => "game", :action => "user_plays_rock"})
 
   get("/paper", {:controller => "game", :action => "user_plays_paper"})
