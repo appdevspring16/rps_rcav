9dee276041593d0715bc671556704efaed30cc7d
stephenandrewberger <stephen.berger@chicagobooth.edu>
2016-04-28 13:36:39 -0500

    Stopping point 1

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,32 @@
 class GameController < ApplicationController
   def rock
+    @computer_move= ["rock", "paper", "scissors"].sample
+
+    if @computer_move== "rock"
+      @result = "tied"
+    end
+
+    if @computer_move== "paper"
+      @result = "lost"
+    end
+
+    if @computer_move== "scissors"
+      @result = "won"
+    end
+
+    render("zebra.html.erb")
+  end
+end
+
+class GameController < ApplicationController
+  def paper
+
+    render("zebra.html.erb")
+  end
+end
+
+class GameController < ApplicationController
+  def scissors
 
     render("zebra.html.erb")
   end
