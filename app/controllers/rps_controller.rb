4d29571878ca52c28eca500149bb0b5e8dc77210
shao313 <shaozhu@chicagobooth.edu>
2016-04-28 16:30:17 -0500

    done

--- app/controllers/rps_controller.rb
+++ app/controllers/rps_controller.rb
@@ -1,19 +1,18 @@
 class RpsController < ApplicationController
 
 
-  def main
-    render ("main.html.erb")
-  end
-
   def rock
     @computer = ["Rock","Paper","Scissors"].sample
     @player = "Rock"
     if @computer == @player
       @result = "You tied!"
+      @label = "label-warning"
     elsif @computer == "Paper"
       @result = "You lost!"
+      @label ="label-danger"
     else
       @result = "You won!"
+      @label="label-success"
     end
     render("rock.html.erb")
   end
@@ -23,10 +22,13 @@ class RpsController < ApplicationController
     @player = "Paper"
     if @computer == @player
       @result = "You tied!"
+      @label = "label-warning"
     elsif @computer == "Scissors"
       @result = "You lost!"
+      @label ="label-danger"
     else
       @result = "You won!"
+      @label="label-success"
     end
     render("paper.html.erb")
   end
@@ -36,10 +38,13 @@ class RpsController < ApplicationController
     @player = "Scissors"
     if @computer == @player
       @result = "You tied!"
+      @label = "label-warning"
     elsif @computer == "Rock"
       @result = "You lost!"
+      @label ="label-danger"
     else
       @result = "You won!"
+      @label="label-success"
     end
     render("scissors.html.erb")
   end
