449520954ce18313173590aa5b5a01d9bdd12f6b
Brian Chingono <chingono@chicagobooth.edu>
2016-04-27 20:40:02 -0500

    Part 1 - complete links and logic

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,9 +3,41 @@ class GameController < ApplicationController
     @computer_move = ["rock", "paper", "scissors"].sample
 
     if @computer_move == "rock"
-      @result == "tied"
+      @result = "tied".to_s
+    elsif @computer_move == "paper"
+      @result = "lose".to_s
+    elsif @computer_move == "scissors"
+      @result = "win!".to_s
     end
 
     render("rock.html.erb")
   end
+
+  def paper
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "paper"
+      @result = "tied".to_s
+    elsif @computer_move == "scissors"
+      @result = "lose".to_s
+    elsif @computer_move == "rock"
+      @result = "win!".to_s
+    end
+    render("paper.html.erb")
+  end
+
+  def scissors
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "scissors"
+      @result = "tied".to_s
+    elsif @computer_move == "rock"
+      @result = "lose".to_s
+    elsif @computer_move == "paper"
+      @result = "win!".to_s
+    end
+
+    render("scissors.html.erb")
+  end
+
 end
