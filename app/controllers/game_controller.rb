8dced8a8e6a82eb08db3eebefa82699630e8ae69
Brian Chingono <chingono@chicagobooth.edu>
2016-04-28 11:14:57 -0500

    Draft 3 (responsive and dynamic images)

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,13 +1,17 @@
 class GameController < ApplicationController
+
   def rock
     @computer_move = ["Rock", "Paper", "Scissors"].sample
 
     if @computer_move == "Rock"
       @result = "tied".to_s
+      @computer_image = "assets/images/rock.png".to_s
     elsif @computer_move == "Paper"
       @result = "lose".to_s
+      @computer_image = "assets/images/paper.png".to_s
     elsif @computer_move == "Scissors"
       @result = "win!".to_s
+      @computer_image = "assets/images/scissors.png".to_s
     end
 
     render("rock.html.erb")
@@ -18,10 +22,13 @@ class GameController < ApplicationController
 
     if @computer_move == "Paper"
       @result = "tied".to_s
+      @computer_image = "assets/images/paper.png".to_s
     elsif @computer_move == "Scissors"
       @result = "lose".to_s
+      @computer_image = "assets/images/scissors.png".to_s
     elsif @computer_move == "Rock"
       @result = "win!".to_s
+      @computer_image = "assets/images/rock.png".to_s
     end
     render("paper.html.erb")
   end
@@ -31,10 +38,13 @@ class GameController < ApplicationController
 
     if @computer_move == "Scissors"
       @result = "tied".to_s
+      @computer_image = "assets/images/scissors.png".to_s
     elsif @computer_move == "Rock"
       @result = "lose".to_s
+      @computer_image = "assets/images/rock.png".to_s
     elsif @computer_move == "Paper"
       @result = "win!".to_s
+      @computer_image = "assets/images/paper.png".to_s
     end
 
     render("scissors.html.erb")
