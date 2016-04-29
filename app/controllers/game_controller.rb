16a98b72ac9ed7a132f2c603f9c3328cc7f1cb80
stephenandrewberger <stephen.berger@chicagobooth.edu>
2016-04-29 10:40:32 -0500

    Final submission
    Browser was loading earlier, but unable to get browser to load anymore.
    Ready for feedback.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -5,49 +5,50 @@ class GameController < ApplicationController
     if @computer_move== "rock"
       @result = "tied"
 
-    if @computer_move== "paper"
+    elsif @computer_move== "paper"
       @result = "lost"
 
-    if @computer_move== "scissors"
+    elsif @computer_move== "scissors"
       @result = "won"
     end
-
-    render("play_rock.html.erb")
   end
-end
+    render("play_rock.html.erb")
+  # end
+  # end
 
-class GameController < ApplicationController
+  # class GameController < ApplicationController
   def paper
     @computer_move=["rock", "paper", "scissors"].sample
 
     if @computer_move == "rock"
       @result= "won"
 
-    if computer_move == "paper"
+    elsif computer_move == "paper"
       @result = "tied"
-  
-    if computer_move == "scissors"
+
+    elsif computer_move == "scissors"
       @result = "lost"
     end
+  end
 
     render("play_paper.html.erb")
-  end
-end
+  # end
+  # end
 
-class GameController < ApplicationController
+  # class GameController < ApplicationController
   def scissors
     @computer_move=["rock", "paper", "scissors"].sample
 
     if @computer_move == "rock"
       @result= "lost"
 
-    if computer_move == "paper"
+    elsif computer_move == "paper"
       @result = "won"
 
-    if computer_move == "scissors"
+    elsif computer_move == "scissors"
       @result = "tied"
     end
-
-    render("play_scissors.html.erb")
   end
+
+  render("play_scissors.html.erb")
 end
