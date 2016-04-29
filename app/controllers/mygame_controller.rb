5f649bda2b854cdf127b3ad87fac79116c57e833
meghankinney <meghankinney@users.noreply.github.com>
2016-04-29 02:01:39 +0000

    logic

--- app/controllers/mygame_controller.rb
+++ app/controllers/mygame_controller.rb
@@ -1,14 +1,47 @@
 class MygameController < ApplicationController
 
+  @computer_move = ['rock','paper','scissors'].sample
+  
   def rock
+    if @computer_move == "rock"
+      @result = "tie"
+     
+    elsif @computer_move == "paper"
+      @result = "lose"
+      
+    elsif @computer_move == "scissors"
+      @result = "win"
+    end
+    
     render("rockgesture.html.erb")
   end
 
+  
   def paper
+    if @computer_move == "rock"
+      @result = "win"
+     
+    elsif @computer_move == "paper"
+      @result = "tie"
+      
+    elsif @computer_move == "scissors"
+      @result = "lose"
+    end
     render("papergesture.html.erb")
   end
 
+  
   def scissors
+    
+    if @computer_move == "rock"
+      @result = "lose"
+     
+    elsif @computer_move == "paper"
+      @result = "win"
+      
+    elsif @computer_move == "scissors"
+      @result = "tie"
+    end
     render("scissorsgesture.html.erb")
   end
 
