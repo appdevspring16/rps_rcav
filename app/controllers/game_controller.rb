2fb7c2d986b3bfdb229e08d7c41f0118f1bc9dd9
Emily Gikow <egikow@chicagobooth.edu>
2016-04-29 11:38:48 -0500

    emily-gikow-update
    but still not working

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,46 +1,56 @@
 class GameController < ApplicationController
 
   def rock
-    # @user_move = params["http://localhost:3000/rock"]
-    @computer_move = ["rock", "paper", "scissors"].sample
+    @computer_move = ["Rock", "Paper", "Scissors"].sample
 
-    if @computer_move == "rock"
-      @result = "tied"
-      # @computer_image = "/assets/images/rock.png"
-    elsif @computer_move == "paper"
-      @result = "lose"
-      render("paper.html.erb")
-    elsif @computer_move == "scissors"
-      @result = "win"
+    if @computer_move == "Rock".to_s
+      @result = "tied".to_s
+      @result_label = "label-warning".to_s
       render("rock.html.erb")
+      # @computer_image = "/assets/images/rock.png"
+    elsif @computer_move == "Paper".to_s
+      @result = "lose".to_s
+      @result_label = "label-danger".to_s
+      render("paper.html.erb")
+    elsif @computer_move == "Scissors".to_s
+      @result = "win".to_s
+      @result_label = "label-success".to_s
+      render("scissors.html.erb")
     end
   end
 
   def paper
-    # @user_move = params["http://localhost:3000/paper"]
-    @computer_move = ["rock", "paper", "scissors"].sample
+    @computer_move = ["Rock", "Paper", "Scissors"].sample
 
-    if @computer_move == "rock"
-      @result = "win"
-    elsif @computer_move == "paper"
-      @result = "tie"
+    if @computer_move == "Rock".to_s
+      @result = "win".to_s
+      @result_label = "label-success".to_s
+      render("rock.html.erb")
+    elsif @computer_move == "Paper".to_s
+      @result = "tie".to_s
+      @result_label = "label-warning".to_s
     render("paper.html.erb")
-    elsif @computer_move == "scissors"
-      @result = "lose"
-      render("paper.html.erb")
+  elsif @computer_move == "Scissors".to_s
+      @result = "lose".to_s
+      @result_label = "label-danger".to_s
+    render("scissors.html.erb")
     end
   end
 
   def scissors
-    # @user_move = params["http://localhost:3000/scissors"]
-    @computer_move = ["rock", "paper", "scissors"].sample
+    @computer_move = ["Rock", "Paper", "Scissors"].sample
 
-    if @computer_move == "rock"
-      @result = "lose"
-    elsif @computer_move == "paper"
-      @result = "win"
-    elsif @computer_move == "scissors"
-      @result = "tie"
+    if @computer_move == "Rock".to_s
+      @result = "lose".to_s
+      @result_label = "label-danger".to_s
+      render("rock.html.erb")
+    elsif @computer_move == "Paper".to_s
+      @result = "win".to_s
+      @result_label = "label-success".to_s
+      render("paper.html.erb")
+    elsif @computer_move == "Scissors".to_s
+      @result = "tie".to_s
+      @result_label = "label-warning".to_s
     render("scissors.html.erb")
     end
   end
