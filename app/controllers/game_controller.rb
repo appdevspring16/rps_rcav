95094c1b3522910e395103c41886a5b17d672dad
Aaron Goltz <aarongoltz@users.noreply.github.com>
2016-04-29 09:48:40 -0500

    Final styling changes.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,41 +1,64 @@
 class GameController < ApplicationController
 
   def rps
-    
+
     tie = "Tie!"
-    win = ["You won!", "Call your mom. Tell her she raised a winner.", "You won, but don't let it go to your head."].sample
-    lose = ["Shoot, you lost.", "You lost. Bummer...", "Computer won, but he's open to a rematch."].sample
+    win = "You won!"
+    lose = "Computer won!"
+
+    tie_color = "info"
+    win_color = "success"
+    lose_color = "danger"
 
     @computer_move = ["rock", "paper", "scissors"].sample
+    @computer_graphic = @computer_move + "-r.png"
 
     if @player_move == @computer_move
       @result = tie
+      @result_class = tie_color
+      @p_move = tie_color
+      @c_move = tie_color
     elsif @player_move == "rock" && @computer_move == "scissors"
       @result = win
+      @result_class = win_color
+      @p_move = win_color
+      @c_move = lose_color
     elsif @player_move == "paper" && @computer_move == "rock"
       @result = win
+      @result_class = win_color
+      @p_move = win_color
+      @c_move = lose_color
     elsif @player_move == "scissors" && @computer_move == "paper"
       @result = win
+      @result_class = win_color
+      @p_move = win_color
+      @c_move = lose_color
     else
       @result = lose
+      @result_class = lose_color
+      @p_move = lose_color
+      @c_move = win_color
     end
 
   end
 
   def rock
     @player_move = "rock"
+    @player_graphic = @player_move + ".png"
     rps
     render("rps.html.erb")
   end
 
   def paper
     @player_move = "paper"
+    @player_graphic = @player_move + ".png"
     rps
     render("rps.html.erb")
   end
 
   def scissors
     @player_move = "scissors"
+    @player_graphic = @player_move + ".png"
     rps
     render("rps.html.erb")
   end
