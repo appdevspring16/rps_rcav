98da740aa6598973268651104e05093ec2c7a5ca
stephenandrewberger <stephen.berger@chicagobooth.edu>
2016-04-29 01:06:07 -0500

    Next stopping point
    Unable to get pages to load all of a sudden...

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -5,10 +5,10 @@ class GameController < ApplicationController
     if @computer_move== "rock"
       @result = "tied"
 
-    elsif @computer_move== "paper"
+    if @computer_move== "paper"
       @result = "lost"
 
-    elsif @computer_move== "scissors"
+    if @computer_move== "scissors"
       @result = "won"
     end
 
@@ -23,10 +23,10 @@ class GameController < ApplicationController
     if @computer_move == "rock"
       @result= "won"
 
-    elsif computer_move == "paper"
+    if computer_move == "paper"
       @result = "tied"
-
-    elsif computer_move == "scissors"
+  
+    if computer_move == "scissors"
       @result = "lost"
     end
 
@@ -41,10 +41,10 @@ class GameController < ApplicationController
     if @computer_move == "rock"
       @result= "lost"
 
-    elsif computer_move == "paper"
+    if computer_move == "paper"
       @result = "won"
 
-    elsif computer_move == "scissors"
+    if computer_move == "scissors"
       @result = "tied"
     end
 
