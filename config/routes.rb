a9472b5e4febcd652ef2e08a191a58f3f43f1215
vparikh0 <vparikh0@chicagobooth.edu>
2016-04-29 00:34:34 -0500

    Almost done
    Result color change remaining

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "game", :action => "rock" })
+
   get("/play/rock", { :controller => "game", :action => "rock" })
 
   get("/play/paper", { :controller => "game", :action => "paper" })
+
+  get("/play/scissor", { :controller => "game", :action => "scissor" })
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
