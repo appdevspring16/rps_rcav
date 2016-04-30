a0795af2c9f41a680bb63ee2ce506a0eaf0e0727
Rosa Rodriguez <rrodrig1@users.noreply.github.com>
2016-04-30 13:25:31 -0500

    Added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get("/", {:controller => "game", :action => "user_plays_rock"})
   get("/rock", {:controller => "game", :action => "user_plays_rock"})
   get("/paper", {:controller => "game", :action => "user_plays_paper"})
   get("/scissors", {:controller => "game", :action => "user_plays_scissors"})
