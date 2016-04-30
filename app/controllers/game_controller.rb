93d59206d8d2ce51478012151e48806da0e39df6
alrarruda20 <aarruda@chicagobooth.edu>
2016-04-30 02:06:04 -0500

    As far as could get

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,49 @@
+class GameController < ApplicationController
+  def user_plays_rock
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @result = "Tie"
+    elsif @computer_move == "paper"
+      @result = "lose"
+    else
+      @result = "Win"
+    end
+
+
+    render("rock.html.erb")
+  end
+
+  def user_plays_paper
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "paper"
+      @result = "Tie"
+    elsif @computer_move == "scissors"
+      @result = "lose"
+    else
+      @result = "Win"
+    end
+
+
+    render("paper.html.erb")
+  end
+
+  def user_plays_scissors
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "scissors"
+      @result = "Tie"
+    elsif @computer_move == "rock"
+      @result = "lose"
+    else
+      @result = "Win"
+    end
+
+
+    render("scissors.html.erb")
+  end
+
+
+
+end
