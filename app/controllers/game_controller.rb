61f79cc9f9782b298d933007934e79764b4dd156
Andrea Ramirez <aramirez@users.noreply.github.com>
2016-05-05 11:09:33 -0500

    Paper set up and logic

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -21,4 +21,26 @@ class GameController < ApplicationController
     render("zebra.html.erb")
   end
 
+  def paper
+
+    @computer_move = ["rock","paper","scissors"].sample
+
+    if @computer_move =="paper"
+      @result = "tied"
+    end
+
+    if @computer_move =="rock"
+      @result = "win"
+    end
+
+    if @computer_move =="scissors"
+      @result = "lose"
+    end
+
+    render("lion.html.erb")
+  end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 get("/rock", { :controller => "game", :action => "rock"})
+get("/paper", { :controller => "game", :action => "paper"})
 
 
 end
