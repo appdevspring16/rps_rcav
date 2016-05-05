9329b68ef0010ef9f032b7befd816201223e3687
Andrea Ramirez <aramirez@users.noreply.github.com>
2016-05-05 11:20:54 -0500

    Scissors set up & logic

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
 get("/rock", { :controller => "game", :action => "rock"})
 get("/paper", { :controller => "game", :action => "paper"})
-
+get("/scissors", { :controller => "game", :action => "scissors"})
 
 end
